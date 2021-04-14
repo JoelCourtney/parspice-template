80f45788b81b6739db7344f906f0cc478a4fa4b8
Joel Courtney <joel.e.courtney@gmail.com>
2021-04-14 15:41:38 -0600

    add gradle run task

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # ParSPICE Java Template
-A basic outline for a parspice project in Java.
+A basic outline for a Gradle Groovy parspice project in Java.
 
 The relevant code can be found in `src/main/java/Main.java`, `src/worker/java/MyWorker.java`, and `build.gradle`.
 
 All other files where generated as-is from a new Gradle Java project in IntelliJ.
+
+A `gradle run` task has been added for convenience.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        final int numIterations = 1000;
-        final int numWorkers = 4;
-
         ParSPICE par = new ParSPICE("build/libs/worker.jar", 50050);
-        List<double[]> results = par.run(new MyWorker(), numIterations, numWorkers);
+        List<double[]> results = par.run(new MyWorker(), 1000, 4);
     }
 }
