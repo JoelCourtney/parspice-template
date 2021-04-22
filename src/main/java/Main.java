0a82c1d456927be8e632cb315f87f9cc88b21211
Joel Courtney <joel.e.courtney@gmail.com>
2021-04-21 22:28:35 -0600

    remove intermediate variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception {
         ParSPICE par = new ParSPICE("build/libs/worker.jar", 50050);
-        var job = (new MyOWorker()).init(4, 1000);
-        List<double[]> results = job.run(par);
+        List<double[]> results = (new MyOWorker()).init(4, 1000).run(par);
     }
 }
